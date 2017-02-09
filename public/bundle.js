3aa8f587d6e8e2451ccaeb4f2796e5ed09ec6f3c
Suresh Shrestha <surestha@outlook.com>
2017-02-09 10:35:11 +0545

    11-egghead-react-building-a-react-js-app-componentwillreceiveprops-and-react-router

--- public/bundle.js
+++ public/bundle.js
@@ -14430,18 +14430,25 @@ var Profile = React.createClass({
   },
   componentDidMount: function componentDidMount() {
     this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
-    var childRef = this.ref.child(this.props.params.username);
+    this.init(this.props.params.username);
+  },
+  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    this.unbind('notes');
+    this.init(nextProps.params.username);
+  },
+  componentWillUnmount: function componentWillUnmount() {
+    this.unbind('notes');
+  },
+  init: function init(username) {
+    var childRef = this.ref.child(username);
     this.bindAsArray(childRef, 'notes');
 
-    helpers.getGithubInfo(this.props.params.username).then(function (data) {
+    helpers.getGithubInfo(username).then(function (data) {
       this.setState({
         bio: data.bio,
         repos: data.repos
       });
     }.bind(this));
-  },
-  componentWillUnmount: function componentWillUnmount() {
-    this.unbind('notes');
   },
   handleAddNote: function handleAddNote(newNote) {
     this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote);
@@ -14494,7 +14501,7 @@ var SearchGithub = React.createClass({
   handleSubmit: function handleSubmit() {
     var username = this.usernameRef.value;
     this.usernameRef.value = '';
-    this.history.pushState(null, "profile/" + username);
+    this.history.pushState(null, "/profile/" + username);
   },
   render: function render() {
     return React.createElement(
